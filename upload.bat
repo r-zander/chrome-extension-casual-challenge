b95f89d42e727227735d421997e7805e2e86902a
R. Zander <r-zander@users.noreply.github.com>
2023-02-28 19:33:59 +0100

    Added upload.bat for automatic uploading.

---
+++ upload.bat
@@ -0,0 +1,2 @@
+rem See https://github.com/fregante/chrome-webstore-upload/blob/main/How%20to%20generate%20Google%20API%20keys.md
+chrome-webstore-upload upload --source .\\dist --extension-id extensionid --client-id client-id.apps.googleusercontent.com --client-secret CL13NT-S3CR37 --refresh-token 1//very-long-refresh-token
