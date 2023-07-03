27bc498833f39a72dca73d67e7773224d70f20ac
R. Zander <r-zander@users.noreply.github.com>
2023-07-04 01:06:05 +0200

    [Pricing Tool] Formatting

--- janik-budgetpoints/budget_point_calculator.py
+++ janik-budgetpoints/budget_point_calculator.py
@@ -7,12 +7,12 @@ pricesFilePath = '.\\AllPrices.json'
 ignoredPricesFilePath = '.\\IgnoredPrices.json'
 outputPath = '..\\data\\card-prices.json'
 
-earliestDate = 20230401 #inclusive
-latestDate   = 20230701 #exclusive
+earliestDate = 20230401  # inclusive
+latestDate = 20230701  # exclusive
 
 illegalBorderColors = ['silver', 'gold']
 
-#cardPrices =
+# cardPrices =
 #	{'Soul Warden': {
 #		'cheapestPerDayAverage': 1.11,
 #		'cheapestPrintAverage': 1.21},
@@ -20,39 +20,33 @@ illegalBorderColors = ['silver', 'gold']
 cardPrices = {}
 
 
-#This is only needed to read from a txt file. You can skip this function, if you already have an array of the decklist ready.
-def readDecklistFromFile (filePath):
-	with open(filePath, 'r', encoding='utf-8') as decklistFile:
-		decklist = decklistFile.read().split('\n')
-		return decklist
-
-
-def getPricesWhithinTimeRange (pricesPerDay):
+def getPricesWhithinTimeRange(pricesPerDay):
 	pricesWithinTimeRange = {}
 	for price in pricesPerDay:
 		dateNumber = 0
 		x = price.split('-')
 		# TODO suspicious
-		dateNumber = int(x[0]) * 10000 + int(x[1]) * 100 + int(x[2]) # Converting a date string 2022-02-01 into the number 20220201 for comparisons
-		if (dateNumber >= earliestDate and dateNumber < latestDate):
-			pricesWithinTimeRange[dateNumber]=(pricesPerDay[price])
+		dateNumber = int(x[0]) * 10000 + int(x[1]) * 100 + int(
+			x[2])  # Converting a date string 2022-02-01 into the number 20220201 for comparisons
+		if dateNumber >= earliestDate and dateNumber < latestDate:
+			pricesWithinTimeRange[dateNumber] = (pricesPerDay[price])
 	return pricesWithinTimeRange
 
 
-def getCheapestPrintAverage (pricesPerPrintings):
+def getCheapestPrintAverage(pricesPerPrintings):
 	allAverages = []
 	for printing in pricesPerPrintings:
-		if (len(pricesPerPrintings[printing]) == 0):
+		if len(pricesPerPrintings[printing]) == 0:
 			continue
 		sum = 0
 		for date in pricesPerPrintings[printing]:
 			sum += pricesPerPrintings[printing][date]
-		allAverages.append(sum/len(pricesPerPrintings[printing]))
+		allAverages.append(sum / len(pricesPerPrintings[printing]))
 	if len(allAverages) == 0:
 		return 0
 	cheapestAverage = allAverages[0]
 	for average in allAverages:
-		if (cheapestAverage > average):
+		if cheapestAverage > average:
 			cheapestAverage = average
 	return cheapestAverage
 
@@ -61,7 +55,7 @@ def getCheapestPerDayAverage(pricesPerPrintings):
 	allMinimumDayPrices = []
 	allDates = []
 	for printing in pricesPerPrintings:
-		if (len(pricesPerPrintings[printing]) == 0):
+		if len(pricesPerPrintings[printing]) == 0:
 			continue
 		for date in pricesPerPrintings[printing]:
 			if date not in allDates:
@@ -78,10 +72,10 @@ def getCheapestPerDayAverage(pricesPerPrintings):
 	sum = 0
 	for average in allMinimumDayPrices:
 		sum += average
-	return sum/len(allMinimumDayPrices)
+	return sum / len(allMinimumDayPrices)
 
 
-def checkForAnomalies(pricesPerPrintings, cardName, uuidAttributes, isDebug = False):
+def checkForAnomalies(pricesPerPrintings, cardName, uuidAttributes, isDebug=False):
 	uuidsToBeRemoved = []
 	for uuid, prices in pricesPerPrintings.items():
 		if len(prices) == 0:
@@ -98,9 +92,12 @@ def checkForAnomalies(pricesPerPrintings, cardName, uuidAttributes, isDebug = Fa
 		if allPricesMatch:
 			if isDebug:
 				if uuid.endswith('-foil'):
-					print('Price anomaly for ' + cardName + ' FOIL (' + str(uuidAttributes[uuid.replace('-foil', '')]) + '): All prices are ' + str(knownPrice))
+					print('Price anomaly for ' + cardName + ' FOIL (' + str(
+						uuidAttributes[uuid.replace('-foil', '')]) + '): All prices are ' + str(knownPrice))
 				else:
-					print('Price anomaly for ' + cardName + ' (' + str(uuidAttributes[uuid]) + '): All prices are ' + str(knownPrice))
+					print(
+						'Price anomaly for ' + cardName + ' (' + str(uuidAttributes[uuid]) + '): All prices are ' + str(
+							knownPrice))
 			uuidsToBeRemoved.append(uuid)
 
 	# Dangerous, see https://docs.google.com/spreadsheets/d/1LKQm2lRDXAFBWJAtGFYlzsqE7Rgx00aA11vpXpDU7DU/edit#gid=429538483 "New 2"
@@ -108,9 +105,9 @@ def checkForAnomalies(pricesPerPrintings, cardName, uuidAttributes, isDebug = Fa
 		pricesPerPrintings.pop(uuid)
 
 
-def calculatePricesForCard (cardName, uuidAttributes, isDebug = False):
+def calculatePricesForCard(cardName, uuidAttributes, isDebug=False):
 	if isDebug: print('calculatePricesForCard ' + cardName)
-	if isDebug: print ('UUIDs and Attributes: ' + str(uuidAttributes))
+	if isDebug: print('UUIDs and Attributes: ' + str(uuidAttributes))
 	rawCardPrices = {}
 	calculatedAveragePrices = {}
 	for printingPriceUUID, attributes in uuidAttributes.items():
@@ -129,46 +126,40 @@ def calculatePricesForCard (cardName, uuidAttributes, isDebug = False):
 		if attributes['hasNonFoil'] == True and 'normal' in cardmarketRetailPrice:
 			rawCardPrices[printingPriceUUID] = getPricesWhithinTimeRange(cardmarketRetailPrice['normal'])
 		if attributes['hasFoil'] == True and 'foil' in cardmarketRetailPrice:
-			rawCardPrices[printingPriceUUID+'-foil'] = getPricesWhithinTimeRange(cardmarketRetailPrice['foil'])
-	if isDebug: print ('rawCardPrices:' + str(rawCardPrices))
+			rawCardPrices[printingPriceUUID + '-foil'] = getPricesWhithinTimeRange(cardmarketRetailPrice['foil'])
+	if isDebug: print('rawCardPrices:' + str(rawCardPrices))
 	# checkForAnomalies(rawCardPrices, cardName, uuidAttributes, isDebug)
-	calculatedAveragePrices['A'] = round(getCheapestPrintAverage(rawCardPrices),2)
-	calculatedAveragePrices['B'] = round(getCheapestPerDayAverage(rawCardPrices),2)
+	calculatedAveragePrices['A'] = round(getCheapestPrintAverage(rawCardPrices), 2)
+	calculatedAveragePrices['B'] = round(getCheapestPerDayAverage(rawCardPrices), 2)
 	cardPrices[cardName] = calculatedAveragePrices
-	if isDebug: print ('calculatedAveragePrices: ' + str(calculatedAveragePrices))
+	if isDebug: print('calculatedAveragePrices: ' + str(calculatedAveragePrices))
 
 
-def getDecklistPrice(decklist, mode = 'A', isDebug = False):
+def getDecklistPrice(decklist, mode='A', isDebug=False):
 	totalDeckPrice = 0
 	for cardName in decklist:
 		if cardName not in cardPrices:
 			calculatePricesForCard(cardName, decklist[cardName], isDebug)
-			if (len(cardPrices) % 100 == 0):
-				print (str(len(cardPrices)) + ' cards done.')
+			if len(cardPrices) % 100 == 0:
+				print(str(len(cardPrices)) + ' cards done.')
 		totalDeckPrice += cardPrices[cardName][mode]
-	print (str(len(cardPrices)) + ' cards done.')
-	return (totalDeckPrice)
+	print(str(len(cardPrices)) + ' cards done.')
+	return totalDeckPrice
 
-def printBothPricesForDecklist (decklistPath):
-	deckFileName = decklistPath.split('\\')[-1]
-	decklist = readDecklistFromFile(decklistPath)
-	cheapestPrintingPrice = getDecklistPrice(decklist)
-	cheapestDayPrice = getDecklistPrice(decklist, 'B')
-	print(deckFileName + ': ' + str(cheapestPrintingPrice)+' | ' +str(cheapestDayPrice) )
 
 # FetchAllPrintings that should be considered for price evaluation
 # {
 #	"Soul Warden": [UUID1, UUID2, ...],
 #	..
 # }
-def getAllCardVersions(getIllegalPrintings = False, isDebug = False):
+def getAllCardVersions(getIllegalPrintings=False, isDebug=False):
 	allCardVersion = {}
 	for mtgSet in rawPrintings:
 		for card in rawPrintings[mtgSet]['cards']:
 			cardName = card['name']
 
-			if isDebug: print('Found in set: '+ mtgSet + ' with UUID ' + card['uuid'])
-			if 'paper' not in card['availability']: # Is this as digital only card?
+			if isDebug: print('Found in set: ' + mtgSet + ' with UUID ' + card['uuid'])
+			if 'paper' not in card['availability']:  # Is this as digital only card?
 				if isDebug: print('Digital only card')
 				continue
 
@@ -194,32 +185,31 @@ def getAllCardVersions(getIllegalPrintings = False, isDebug = False):
 	# 	break
 	return allCardVersion
 
-print ('Untap, Upkeep, Draw!')
+
+print('Untap, Upkeep, Draw!')
 
 print('Reading printings')
 with open(printingsFilePath, 'r', encoding='utf-8') as f:
 	rawPrintings = json.load(f)['data']
 
 print('Reading Prices')
-with open (pricesFilePath, 'r', encoding='utf-8') as f:
+with open(pricesFilePath, 'r', encoding='utf-8') as f:
 	rawPrices = json.load(f)['data']
 
 print('Reading Ignore list')
-with open (ignoredPricesFilePath, 'r', encoding='utf-8') as f:
+with open(ignoredPricesFilePath, 'r', encoding='utf-8') as f:
 	rawIgnores = json.load(f)
 
 print('Building up Card Dictionary')
 # Add basics as free cards
 basics = ['Plains', 'Island', 'Swamp', 'Mountain', 'Forest', 'Wastes']
-basicPrices = {}
-basicPrices['A'] = 0
-basicPrices['B'] = 0
+basicPrices = {'A': 0, 'B': 0}
 for basic in basics:
-	cardPrices[basic]=basicPrices
+	cardPrices[basic] = basicPrices
 
 # Create a "decklist" with every card in existance
 allCards = getAllCardVersions()
-print ('Done building Card Dictionary: ' + str(len(allCards)))
+print('Done building Card Dictionary: ' + str(len(allCards)))
 
 # Uncomment to look at a single card in detail
 # allCards = {
@@ -227,11 +217,11 @@ print ('Done building Card Dictionary: ' + str(len(allCards)))
 # }
 # getDecklistPrice(allCards, 'A', True)
 
-print ('Calculating budget points')
+print('Calculating budget points')
 getDecklistPrice(allCards)
 
-print ('Writing card-prices.json')
+print('Writing card-prices.json')
 with open(outputPath, 'w', encoding='utf-8') as f:
 	f.write(json.dumps(cardPrices))
 
-print ('All done. Ending now.')
+print('All done. Ending now.')
